24e85f2a614fedbf9060629f58a781063945106b
Victor Wang <victoongo@gmail.com>
2014-11-26 09:47:58 -0500

    assign w8

--- informatics/assignments.py
+++ informatics/assignments.py
@@ -153,3 +153,144 @@ for line in fh:
         #print ct
 av = x / ct
 print "Average spam confidence:", av
+
+
+## Week 8
+# Lecture
+x = 2
+x = 4
+print x
+print [1,24,76]
+print ['red','yellow','blue']
+print ['red',24,43.2]
+print [1,[3,4],32]
+print []
+fruit = 'Banana'
+fruit[0] = 'b'
+print fruit
+x = fruit.lower()
+lotto = [1,2,3,5,9]
+lotto[2] = 34
+friends = ['Jose','Glenn','Sally']
+print len(friends)
+print range(len(friends))
+for friend in friends:
+    print 'happy', friend
+for i in range(len(friends)):
+    friend = friends[i]
+    print 'happy', friend
+a = [1,2,3]
+b = [4,5,6]
+c = a + b
+print c
+t = [2,23,34,56,67]
+t[1:3]
+x = list()
+type(x)
+dir(x)
+
+stuff = list()
+stuff.append('book')
+stuff.append(99)
+print stuff
+2 in t
+3 not in t
+friends.sort()
+print friends
+print len(t)
+print max(t)
+print min(t)
+print sum(t)
+print sum(t)/len(t)
+
+total = 0 
+count = 0
+while True:
+    inp = raw_input('Enter a number:')
+    if inp == 'done': break
+    value = float(inp)
+    total = total + value
+    count = count + 1
+
+average = total / count
+print 'Average:', average
+
+numlist = list()
+while True:
+    inp = raw_input('Enter a number:')
+    if inp == 'done':break
+    value = float(inp)
+    numlist.append(value)
+
+average = sum(numlist) / len(numlist)
+print 'Average:', average
+
+abc = 'With three wods'
+stuff = abc.split()
+print stuff
+print len(stuff)
+print stuff[0]
+for w in stuff:
+    print w
+    
+line = 'A lot          of spaces'
+etc = line.split()
+print etc
+line = 'first;second;third'
+thing = line.split()
+print thing
+print len(thing)
+thing = line.split(';')
+print thing
+print len(thing)
+
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From '):continue
+    words = line.split()
+    print words[2]
+    email = words[1]
+    pieces = email.split('@')
+    url = pieces[1]
+    print url
+
+
+# Assignment 8.4
+# 8.4 Open the file romeo.txt and read it line by line. 
+# For each line, split the line into a list of words using the split() function. 
+# The program should build a list of words. For each word on each line check to 
+# see if the word is already in the list and if not append it to the list. 
+# When the program completes, sort and print the resulting words in alphabetical order.
+# You can download the sample data at http://www.pythonlearn.com/code/romeo.txt
+fname = raw_input("Enter file name: ")
+fh = open('romeo.txt')
+lst = list()
+for line in fh:
+    words = line.split()
+    for word in words:
+        if word in lst:continue
+        lst.append(word)
+lst.sort()
+print lst
+#print line.rstrip()
+
+# Assignment 8.5
+# 8.5 Open the file mbox-short.txt and read it line by line. When you find a line 
+# that starts with 'From ' like the following line:
+# From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+# You will parse the From line using split() and print out the second word in the 
+# line (i.e. the entire address of the person who sent the message). Then print out a count at the end.
+# Hint: make sure not to include the lines that start with 'From:'.
+# You can download the sample data at http://www.pythonlearn.com/code/mbox-short.txt
+fname = raw_input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+for line in fh:
+    if line.startswith('From '):
+        words = line.split()
+        print words[1]
+        count = count + 1
+print "There were", count, "lines in the file with From as the first word"
